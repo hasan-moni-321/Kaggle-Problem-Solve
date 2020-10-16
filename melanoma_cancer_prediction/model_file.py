2a163a02e9eb6b9d3f83c6e5037a1091f9e187a6
Hasan Moni <62002387+hasan-moni-321@users.noreply.github.com>
2020-10-16 09:27:16 +0600

    Add files via upload

---
+++ melanoma_cancer_prediction/model_file.py
@@ -0,0 +1,22 @@
+import pretrainedmodels
+import torch 
+import torch.nn as nn 
+
+class SEResNext50_32x4d(nn.Module):
+    def __init__(self, pretrained="imagenet"):
+        super(SEResNext50_32x4d, self).__init__()
+        self.model = pretrainedmodels.__dict__[
+            "se_resnext50_32x4d"
+        ](pretrained=pretrained)
+        self.out = nn.Linear(2048, 1)
+    
+    def forward(self, image, targets):
+        bs, _, _, _ = image.shape
+        x = self.model.features(image)
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = x.reshape(bs, -1)
+        out = self.out(x)
+        loss = nn.BCEWithLogitsLoss()(
+            out, targets.reshape(-1, 1).type_as(out)
+        )
+        return out, loss
